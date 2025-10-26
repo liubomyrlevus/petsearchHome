4dc37773f7af4a4477783282c850878f93614b11
Pavlo Patrylo <patrylopavlyk@gmail.com>
2025-10-26 18:40:05 +0200

    Configure MudBlazor and temp disable BLL/DAL services

--- src/PetSearchHome.Presentation/MauiProgram.cs
+++ src/PetSearchHome.Presentation/MauiProgram.cs
@@ -1,11 +1,15 @@
-﻿using Microsoft.Extensions.Logging;
+﻿
+using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
-using MediatR;
-using PetSearchHome.DAL;
+using Microsoft.Extensions.Logging;
+// Додано using для сервісів MudBlazor
+using MudBlazor.Services;
 using PetSearchHome.BLL;
-using PetSearchHome.DAL.Repositories;
-using PetSearchHome.BLL.Services;
+//using PetSearchHome.BLL.Services;
+using PetSearchHome.DAL;
+//using PetSearchHome.DAL.Repositories;
+using System;
 
 namespace PetSearchHome.Presentation;
 
@@ -24,19 +28,29 @@ public static class MauiProgram
 
         var connString = config.GetConnectionString("DefaultConnection");
 
-        // PostgreSQL
-        builder.Services.AddDbContext<AppDbContext>(options =>
-            options.UseNpgsql(connString));
+        //// PostgreSQL
+        //builder.Services.AddDbContext<AppDbContext>(options =>
+        //    options.UseNpgsql(connString));
 
-        // DAL
-        builder.Services.AddScoped<IUserRepository, UserRepository>();
-        builder.Services.AddScoped<IListingRepository, ListingRepository>();
+        //// DAL
+        //builder.Services.AddScoped<IUserRepository, UserRepository>();
+        //builder.Services.AddScoped<IListingRepository, ListingRepository>();
 
-        // BLL
-        builder.Services.AddMediatR(cfg =>
-            cfg.RegisterServicesFromAssembly(typeof(SomeHandler).Assembly));
-        builder.Services.AddScoped<IUserService, UserService>();
-        builder.Services.AddScoped<IListingService, ListingService>();
+        //// BLL
+        //builder.Services.AddMediatR(cfg =>
+        //    cfg.RegisterServicesFromAssembly(typeof(SomeHandler).Assembly));
+        //builder.Services.AddScoped<IUserService, UserService>();
+        //builder.Services.AddScoped<IListingService, ListingService>();
+
+        // --- Додано сервіси для Blazor та MudBlazor ---
+
+        // 1. Потрібно для запуску Blazor всередині MAUI
+        builder.Services.AddMauiBlazorWebView();
+
+        // 2. Потрібно для роботи компонентів MudBlazor
+        builder.Services.AddMudServices();
+
+        // ------------------------------------------------
 
 #if DEBUG
         builder.Logging.AddDebug();
